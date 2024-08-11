6d2caaac633c476a831da2d20dbeab0ae25cdc54
Youssef Hassany <youssefhassany22@gmail.com>
2024-08-11 19:06:15 +0300

    setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import DarkModeToggle from "./components/shared/DarkModeToggle ";
 
 function App() {
   return (
-    <>
-      <DarkModeToggle />
-
-      <div className="bg-base-light dark:bg-base-dark text-highlight-light dark:text-highlight-dark h-screen">
-        This background color will change based on the theme.
-      </div>
-    </>
+    <main className="w-screen min-h-screen bg-base">
+      <h1 className="text-secondary text-center">Hello GI-GG</h1>
+    </main>
   );
 }
 
