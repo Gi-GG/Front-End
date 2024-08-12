f9458ba5243762eea41d36016f6f02a15c756fca
Youssef Hassany <youssefhassany22@gmail.com>
2024-08-12 05:15:25 +0300

    handling cookies and error messages

--- src/hooks/auth/useSignIn.ts
+++ src/hooks/auth/useSignIn.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { baseUrl } from "../../utils/baseUrl";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 interface User {
   email: string;
@@ -13,13 +14,17 @@ const useSignIn = () => {
     mutationFn: async (user: User) => {
       const response = await axios.post(`${baseUrl}/auth/login`, user);
       console.log(response.data);
+      Cookies.set("token", response.data.token, { expires: 7 });
       return response.data;
     },
     onSuccess: () => {
       // Handle success (e.g., show a success message)
     },
-    onError: () => {
-      // Handle error (e.g., show an error message)
+    onError: (error: any) => {
+      // Pass the error message to the component
+      const errorMessage =
+        error.response?.data?.message || "An unexpected error occurred.";
+      throw new Error(errorMessage);
     },
   });
 };
